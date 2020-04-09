08197b0e5eef0c06c8e43c6de54a0198fd35ba04
Kræn Glintborg Bech-Petersen <info@kraenbech.dk>
2020-04-10 00:46:20 +0200

    More deletes

--- wp-content/themes/oesekirke2015/searchform.php
+++
@@ -1,9 +0,0 @@
-  <form method="get" id="searchform" class="search" role="search" action="<?php bloginfo('url'); ?>/">
-    <div class="form-group input-group">
-      <label for="s" class="sr-only">Søg</label>
-      <input type="text" name="s" id="s" class="form-control" placeholder="Søg">
-      <span class="input-group-btn">
-        <button type="submit" class="btn btn-default" id="searchSubmit"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-      </span>
-    </div>
-  </form>
